3d3e209d5b4b834250628c91039376eb50751612
Marcin <marcin.kostrzemski@gmail.com>
2023-12-23 16:19:12 +0100

    web browser fixes & favicon

--- src/models/gasPlanet.tsx
+++ src/models/gasPlanet.tsx
@@ -1,7 +1,5 @@
-import PlanetRing from "../interfaces/planetRing";
 import hexColor from "../types/hexColor";
 import Planet from "./planet";
-import Satellite from "./satellite";
 
 export default class GasPlanet extends Planet{
     color: hexColor = '#ffffff';
